327092afdd9c7d568f2dd1006c0e1bddebe9d88b
Jaimin Patel <jaiminpatel211290@gmail.com>
2018-12-01 13:31:37 +0530

    Update test.js

--- test.js
+++ test.js
@@ -5,10 +5,15 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+  
 var _react = React;
 
 var _react2 = _interopRequireDefault(_react);
 
+  
+
+  
 var _reactNative = ReactNative;
 
   return _react2.default.createElement(
