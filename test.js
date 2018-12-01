db1f9b097cec970726e592a5cd45be80b085aacd
Jaimin Patel <jaiminpatel211290@gmail.com>
2018-12-01 13:28:41 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,20 @@
+(function main(React,ReactNative, require) {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = React;
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactNative = ReactNative;
+
+  return _react2.default.createElement(
+          _reactNative.Text,
+          {},
+          ['Nice Once Again. Thank you !!']
+      );
+      
+})
