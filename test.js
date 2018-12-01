ad2ca0fa8b591de4dfd1d147e968e12b9913393c
Jaimin Patel <jaiminpatel211290@gmail.com>
2018-12-01 14:06:54 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-(function main(React,ReactNative, require) {
+(function main(React,ReactNative,componentState, require) {
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -18,7 +18,7 @@ var _reactNative = ReactNative;
    
   return _react2.default.createElement(
           _reactNative.Text,
-          { onPress : function () { alert(this.state.existingUserId) }},
+          { onPress : function () { alert(componentState.state.existingUserId) }},
           ['Nice Once Again. Thank you !!']
       );
       
