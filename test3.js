e04299219ff99a83f2d2aa5ca6ba01bee49e0c85
Jaimin Patel <jaiminpatel211290@gmail.com>
2018-12-03 18:10:19 -0800

    Create test3.js

---
+++ test3.js
@@ -0,0 +1,25 @@
+(function main(React, ReactNative, componentState, require) {
+        'use strict';
+
+        Object.defineProperty(exports, "__esModule", {
+            value: true
+        });
+
+        function _interopRequireDefault(obj) {
+            return obj && obj.__esModule ? obj : {
+                default: obj
+            };
+        }
+
+        var _react = React;
+
+        var _react2 = _interopRequireDefault(_react);
+
+
+
+
+        var _reactNative = ReactNative;
+
+        return _react2.default.createElement(_reactNative.View, {}, []);
+
+})
