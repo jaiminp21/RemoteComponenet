eb6b1258d31ecf5fd392257a140fc5e82773f8b5
Jaimin Patel <jaiminpatel211290@gmail.com>
2018-12-18 15:02:58 -0800

    Create appdata.js

---
+++ appdata.js
@@ -0,0 +1,13 @@
+(function main(React, ReactNative, componentState, NavigationRouter, require) {
+    
+    var react_1 = React;
+
+    var _reactNative = ReactNative;
+
+    var root = this;
+  
+    return react_1.createElement(_reactNative.Root, {},
+        [
+          react_1.createElement(NavigationRouter,{},[])
+        ])
+})
