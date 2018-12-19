4f868097467b718a89bf3592247595e085e27a1e
Jaimin Patel <jaiminpatel211290@gmail.com>
2018-12-19 08:15:01 +0530

    Create router4.js

---
+++ router4.js
@@ -0,0 +1,24 @@
+(function main(React, ReactNative,Root,NavigationRouter,createReactClass, require) {
+
+
+    var react_1 = React;
+    var _reactNative = ReactNative;
+    //var root = this;
+      
+    
+   
+        return react_1.createElement(Root, { "id" :"rootooooo" },
+        [
+        
+        react_1.createElement(NavigationRouter,
+            {
+                "id": "M_layoutoooooo",
+                "text": null,
+                "notifyType": {}
+            }, [])
+        ]
+        );
+ 
+
+  
+})
