982f756d559be307045f4d4e6fc0a1dcb873309b
Jaimin Patel <jaiminpatel211290@gmail.com>
2018-12-19 08:10:34 +0530

    Create router3.js

---
+++ router3.js
@@ -0,0 +1,29 @@
+(function main(React, ReactNative,Root,NavigationRouter,createReactClass, require) {
+
+
+    var react_1 = React;
+    var _reactNative = ReactNative;
+    var root = this;
+      
+    
+   // return createReactClass({
+   // render: function() {
+        return react_1.createElement(Root, { "id" :"roott" },
+        [
+        
+        react_1.createElement(NavigationRouter,
+            {
+                "id": "M_layout",
+                "text": null,
+                "notifyType": {}
+            }, [])
+        ]
+         )
+    //  }
+   //});
+ 
+     
+//  return Component;
+
+  
+})
