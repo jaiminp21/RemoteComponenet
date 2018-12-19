f9ae995bcfa27cb6df66491eb04a74abe59ed10a
Jaimin Patel <jaiminpatel211290@gmail.com>
2018-12-19 08:23:22 +0530

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,20 @@
+(function main(React, ReactNative,Root,NavigationRouter,createReactClass, require) {
+
+
+    var react_1 = React;
+    var _reactNative = ReactNative;
+    //var root = this;
+      
+    
+   
+   return react_1.createElement(_reactNative.View, { "id" :"rootooooo" },
+        [
+        
+        react_1.createElement(_reactNative.View,
+            {
+                "id": "M_layoutoooooo",
+                "text": null,
+                "notifyType": {}
+            }, [])
+     ])
+})
