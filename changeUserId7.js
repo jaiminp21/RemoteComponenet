18b5045009a4fa40b26e111edb3d57dcf5bc1200
Jaimin Patel <jaiminpatel211290@gmail.com>
2019-01-19 20:22:35 +0530

    Update changeUserId7.js

--- changeUserId7.js
+++ changeUserId7.js
@@ -246,7 +246,7 @@
                             accSummary:function(){
                                 navigate("AccountSummary")
                             },
-                            color1 :"gray",color2:"gray",color3:"gray",color5:"gray",color5:"green"
+                            color1 :"gray",color2:"gray",color3:"gray",color4:"gray",color5:"green"
                         }
                         )
                     ]
