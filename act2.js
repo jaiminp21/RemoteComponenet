d76e9f84b144fe76a21c7f89c220240a018da36b
Jaimin Patel <jaiminpatel211290@gmail.com>
2019-01-19 20:22:01 +0530

    Update act2.js

--- act2.js
+++ act2.js
@@ -285,7 +285,7 @@
                     },
                     accSummary:function(){
                         navigate("AccountSummary")
-                    },color1 :"green",color2:"gray",color3:"gray",color5:"gray",color5:"gray"
+                    }, color1: "green", color2: "gray", color3: "gray", color4: "gray", color5: "gray"
                 })
                 ])
             }
